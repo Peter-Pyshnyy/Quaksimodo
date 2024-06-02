57eb9d8394647d5e6d96ea3f162abe6b7d942be6
jontov <jovob@web.de>
2024-06-02 13:22:53 +0200

    mathe eingebaut

---
+++ data/levels.gd
@@ -0,0 +1,13 @@
+class_name Levels
+
+const Database = {
+	"1" : {
+		"enemy_health" : 3,
+		"questions" : {
+			"1" : {
+				"q" : "Whats 1 + 2?",
+				"a" : "3"
+			}
+		}
+	} 
+}
--- fight.gd
+++ fight.gd
@@ -2,19 +2,36 @@ extends Node2D
 
 @onready var healthbar = $HealthBar
 var rng = RandomNumberGenerator.new()
-var player_health
+var enemy_health
+var current_level
+var current_question_number
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player_health = 100
-	healthbar.init_health(player_health)
+	current_level = 1
+	current_question_number = 1
+	enemy_health = Levels.Database[str(current_level)].enemy_health
+	print(enemy_health)
+	healthbar.init_health(enemy_health)
+	load_question()
 	$Sprite2D/AnimationPlayer.play("idle")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func load_question():
+	$Question.text = Levels.Database[str(current_level)].questions[str(current_question_number)].q
+	$Button.disabled = true
+	
 
 func _on_attack_button_pressed():
 	print("works")
-	player_health = player_health - rng.randf_range(4.0, 10.0)
-	healthbar.health = player_health
+	enemy_health = enemy_health - rng.randf_range(4.0, 10.0)
+	healthbar.health = enemy_health
+
+
+func _on_answer_text_submitted(new_text):
+	if(new_text == Levels.Database[str(current_level)].questions[str(current_question_number)].a):
+		$Button.disabled = false
+	else:
+		print("wrong")
