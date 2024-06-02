24d5e86e3060b7da7f64da5695e835c1a565c487
jontov <jovob@web.de>
2024-06-02 17:42:00 +0200

    added new input layout without functionality

--- data/levels.gd
+++ data/levels.gd
@@ -1,13 +1,19 @@
 class_name Levels
 
-const Database = {
+const LevelDatabase = {
 	"1" : {
 		"enemy_health" : 3,
-		"questions" : {
-			"1" : {
-				"q" : "Whats 1 + 2?",
-				"a" : "3"
-			}
-		}
+		
 	} 
 }
+const QuestionDatabase = {
+			"1" : {
+				"f" : "Whats 1 + 2?",
+				"q1" : "Wo befindet sich die Nullstelle?",
+				"a1" : "Dort",
+				"q2" : "Wo befindet sich die Nullstelle?",
+				"a2" : "Dort",
+				"q3" : "Wo befindet sich die Nullstelle?",
+				"a3" : "Dort"
+			}
+		}
