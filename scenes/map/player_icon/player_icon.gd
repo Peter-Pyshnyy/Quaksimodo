44588e5f47e36982169232b04aab276e08f0e753
Peter-Pyshnyy <pyshnyy.peter@gmail.com>
2024-06-02 21:58:34 +0200

    changed map visuals
    - squares now appear when you go on them
    - changed camera settings

---
+++ scenes/map/player_icon/player_icon.gd
@@ -0,0 +1,31 @@
+extends CharacterBody2D
+
+var currPos: Vector2
+@onready var map = $"../.."
+
+func _input(event):
+	var input_movement: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	currPos = self.position
+	
+	if(input_movement == Vector2.ZERO):
+		return
+	
+	if(input_movement.x != 0 && input_movement.y != 0):
+		return
+	
+	if(!check_path(currPos + 16*input_movement)):
+		return
+	
+	self.position += 16*input_movement
+	var square = Vector2i(self.position/16)
+	square.y *= -1
+	map.active_sqr = map.squares_dict[square]
+	map.draw_active_square()
+	
+	
+
+func check_path(pos: Vector2i) -> bool:
+	var grid_coords = round(pos/16)
+	grid_coords.y *= -1
+
+	return map.squares_dict.has(grid_coords)
