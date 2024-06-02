02c89de6c857ff475b1f4cae2a70fb855a4e5707
Peter-Pyshnyy <pyshnyy.peter@gmail.com>
2024-06-02 16:39:43 +0200

    Squashed commit of the following:
    commit dc7fdd2440830ff28052a39917b5fc0bf6fba8ff
    Author: sylvie <lennard.gnuechtel@mailbox.tu-dresden.de>
    Date:   Sun Jun 2 15:17:12 2024 +0200
    
        added customization to dialogue box
    
    commit 3bd31c827266fe1fd3616f3a9cecc4889aebb0ac
    Author: sylvie <lennard.gnuechtel@mailbox.tu-dresden.de>
    Date:   Fri May 31 19:03:47 2024 +0200
    
        adds basic dialogue to tutorial node

---
+++ tutorial.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	DialogueManager.show_dialogue_balloon(load("res://dialogue/tutorial.dialogue"),"tutorial_start")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
