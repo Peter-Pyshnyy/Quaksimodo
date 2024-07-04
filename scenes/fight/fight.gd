37abb456e099b8e27365a98b74494c3769495147
Jona <jovob@web.de>
2024-07-04 22:29:15 +0200

    generate_function() hinzugefügt

--- scenes/fight/fight.gd
+++ scenes/fight/fight.gd
@@ -21,6 +21,7 @@ func _ready():
 	$Enemy/AnimationPlayer.play("idle")
 	$Frog/AnimationPlayer.play("idle")
 	$background/AnimationPlayer.play("idle")
+	generate_function()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -28,7 +29,7 @@ func _process(delta):
 	pass
 
 func load_questions():
-	$Function.text = "Function: " + Levels.QuestionDatabase[str(current_question_number)].f
+	$Function.text = "Function: " + generate_function()
 	$Question.text = Levels.QuestionDatabase[str(current_question_number)].q1
 	$Question2.text = Levels.QuestionDatabase[str(current_question_number)].q2
 	$Question3.text = Levels.QuestionDatabase[str(current_question_number)].q3
@@ -117,5 +118,61 @@ func _on_answer_2_focus_exited():
 func _on_answer_3_focus_exited():
 	anwer_to_q3 = $Answer3.text
 
-
-
+func generate_function():
+	#generiert ein polynom vom grad 1 bis 3 mit den zufälligen parametern a, b und c
+	var rng = RandomNumberGenerator.new()
+	var a = rng.randi_range(-50, 50)
+	var b = rng.randi_range(-50, 50)
+	var c = rng.randi_range(-50, 50)
+	var d = rng.randi_range(-50, 50)
+	var degree = rng.randi_range(1, 3)
+	print(degree)
+	var function = "f(x) = "
+	if(a != 0 && degree >= 0):
+		if(a == 1):
+			function += "1"
+		elif(a == -1):
+			function += "-1"
+		else:
+			function += str(a)
+			
+	if(b != 0 && degree >= 1):
+		if(b > 0):
+			function += " +"
+		else: 
+			function += " "
+		
+		if(b == 1):
+			function += "x"
+		elif(b == -1):
+			function += "-x"
+		else:
+			function += str(b) + "x"
+			
+	if(c != 0 && degree >= 2):
+		if(c > 0):
+			function += " +"
+		else: 
+			function += " "
+		
+		if(c == 1):
+			function += "x²"
+		elif(c == -1):
+			function += "-x²"
+		else:
+			function += str(c) + "x²"
+	if(d != 0 && degree >= 3):
+		if(d > 0):
+			function += " +"
+		else: 
+			function += " "
+		
+		if(d == 1):
+			function += "x³"
+		elif(d == -1):
+			function += "-x³"
+		else:
+			function += str(d) + "x³"
+			
+			
+	return function
