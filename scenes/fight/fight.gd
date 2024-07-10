29e7555ffb01e8bb1e65de50470aa0f94ac1b66b
Jona <jovob@web.de>
2024-07-10 14:00:40 +0200

    option buttons added

--- scenes/fight/fight.gd
+++ scenes/fight/fight.gd
@@ -5,6 +5,9 @@ extends Node2D
 @onready var input1 = $Answer
 @onready var input2 = $Answer2
 @onready var input3 = $Answer3
+@onready var option1 = $OptionButton
+@onready var option2 = $OptionButton2
+@onready var option3 = $OptionButton3
 var rng = RandomNumberGenerator.new()
 var enemy_health
 var frog_health
@@ -23,11 +26,10 @@ func _ready():
 	healthbar_enemy.init_health(enemy_health)
 	healthbar_frog.init_health(10)
 	healthbar_frog.health = frog_health
-	load_questions()
 	$Enemy/AnimationPlayer.play("idle")
 	$Frog/AnimationPlayer.play("idle")
 	$background/AnimationPlayer.play("idle")
-	generate_function()
+
 	
 	gen_new_questions()
 
@@ -35,13 +37,9 @@ func _ready():
 func _process(delta):
 	pass
 
-func load_questions():
-	$Function.text = "Function: " + generate_function()
-	$Question.text = Levels.QuestionDatabase[str(current_question_number)].q1
-	$Question2.text = Levels.QuestionDatabase[str(current_question_number)].q2
-	$Question3.text = Levels.QuestionDatabase[str(current_question_number)].q3
-
 func gen_new_questions():
+	input1.visible = true
+	reset_option_visible()
 	question = Question.new(current_level)
 	current_function = question.fn
 	
@@ -66,6 +64,7 @@ func gen_new_questions():
 		input3.visible = true
 
 func _on_attack_button_pressed():
+	print(anwer_to_q1)
 	$Answer.grab_focus()
 	if(question.give_answer(0, anwer_to_q1)):
 		print("right")
@@ -122,7 +121,7 @@ func _on_attack_button_pressed():
 			current_question_number = 1
 		else:
 			current_question_number += 1
-		load_questions()
+	
 
 func hurt_frog():
 	frog_health = frog_health - 1
@@ -151,62 +150,40 @@ func _on_answer_2_focus_exited():
 	
 func _on_answer_3_focus_exited():
 	anwer_to_q3 = $Answer3.text
+	
+func turn_input_to_option(options: Array, input_nr: int):
+	var option_button
+	var input
+	match input_nr:
+		1:
+			option_button = option1
+			input = input1
+		2: 
+			option_button = option2
+			input = input2
+		3: 
+			option_button = option3
+			input = input3
+	
+	option_button.visible = true
+	input.visible = false
+	option_button.clear()
+	for item in options:
+		option_button.add_item(item)
 
-func generate_function():
-	#generiert ein polynom vom grad 1 bis 3 mit den zufälligen parametern a, b und c
-	var rng = RandomNumberGenerator.new()
-	var a = rng.randi_range(-50, 50)
-	var b = rng.randi_range(-50, 50)
-	var c = rng.randi_range(-50, 50)
-	var d = rng.randi_range(-50, 50)
-	var degree = rng.randi_range(1, 3)
+func reset_option_visible():
+	option1.visible = false
+	option2.visible = false
+	option3.visible = false
 
-	var function = "f(x) = "
-	if(a != 0 && degree >= 0):
-		if(a == 1):
-			function += "1"
-		elif(a == -1):
-			function += "-1"
-		else:
-			function += str(a)
-			
-	if(b != 0 && degree >= 1):
-		if(b > 0):
-			function += " +"
-		else: 
-			function += " "
-		
-		if(b == 1):
-			function += "x"
-		elif(b == -1):
-			function += "-x"
-		else:
-			function += str(b) + "x"
-			
-	if(c != 0 && degree >= 2):
-		if(c > 0):
-			function += " +"
-		else: 
-			function += " "
-		
-		if(c == 1):
-			function += "x²"
-		elif(c == -1):
-			function += "-x²"
-		else:
-			function += str(c) + "x²"
-	if(d != 0 && degree >= 3):
-		if(d > 0):
-			function += " +"
-		else: 
-			function += " "
-		
-		if(d == 1):
-			function += "x³"
-		elif(d == -1):
-			function += "-x³"
-		else:
-			function += str(d) + "x³"
-			
-			
-	return function
+
+func _on_option_button_item_selected(index):
+	anwer_to_q1 = option1.get_item_text(index)
+
+
+func _on_option_button_2_item_selected(index):
+	anwer_to_q2 = option2.get_item_text(index)
+
+
+func _on_option_button_3_item_selected(index):
+	anwer_to_q3 = option3.get_item_text(index)
