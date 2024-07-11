4c6f6e8e5c0d58fa0c59ecd3a5d91be3366add81
Sylvie <147659020+S-y1vie@users.noreply.github.com>
2024-07-11 14:30:47 +0200

    added input tutorial

--- scenes/tutorial/tutorial.gd
+++ scenes/tutorial/tutorial.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	DialogueManager.show_dialogue_balloon(load("res://dialogue/tutorial.dialogue"),"tutorial_nullstellen")
-	print("HELLO")
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
